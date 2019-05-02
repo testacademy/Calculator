cd32d0b1916c0caa5a6338a43de3b1b892c81a6f
testacademy <tutestacademy@gmail.com>
2019-05-02 13:45:17 +0300

    Initial project

---
+++ Calc/Calculator.cs
@@ -0,0 +1,28 @@
+﻿namespace Calc
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+        public int Subtract()
+        {
+            return FirstNumber - SecondNumber;
+        }
+
+        public int Multiply()
+        {
+            return FirstNumber * SecondNumber;
+        }
+
+        public int Divide()
+        {
+            return FirstNumber / SecondNumber;
+        }
+    }
+}
